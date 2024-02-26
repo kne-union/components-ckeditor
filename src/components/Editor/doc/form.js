897e2ba760132db28dac4fd90baef54796267f4a
zhipenglin <sunandmoon001@163.com>
2024-02-26 13:03:31 +0800

    添加新示例

--- src/components/Editor/doc/form.js
+++ src/components/Editor/doc/form.js
@@ -1,41 +1,3 @@
-
-# Editor
-
-
-### 概述
-
-富文本编辑器ckeditor的封装
-
-
-### 示例
-
-#### 示例代码
-
-- 这里填写示例标题
-- 这里填写示例说明
-- _Editor(@components/Editor)
-
-```jsx
-const {default: Editor} = _Editor;
-const { useState } = React;
-const BaseExample = () => {
-    const [value, onChange] = useState("<p>请输入内容</p>");
-    return <div>
-        <Editor.Field value={value} onChange={onChange}/>
-        <div>预览：</div>
-        <Editor.Content value={value}/>
-    </div>;
-};
-
-render(<BaseExample/>);
-
-```
-
-- 在表单中使用
-- 在表单中使用
-- _Editor(@components/Editor),remoteLoader(@kne/remote-loader)
-
-```jsx
 const { default: CKEditor } = _Editor;
 const { createWithRemoteLoader } = remoteLoader;
 const BaseExample = createWithRemoteLoader({
@@ -66,12 +28,3 @@ const BaseExample = createWithRemoteLoader({
 });
 
 render(<BaseExample />);
-
-```
-
-
-### API
-
-|属性名|说明|类型|默认值|
-|  ---  | ---  | --- | --- |
-
